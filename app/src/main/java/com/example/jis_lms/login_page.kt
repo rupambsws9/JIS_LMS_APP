0bc01c1536fa36c31439235a9546e512da8a9630
subhasish-santra <subhasishsantra78@gmail.com>
2025-03-26 15:40:17 +0530

    initial complete

--- app/src/main/java/com/example/jis_lms/login_page.kt
+++ app/src/main/java/com/example/jis_lms/login_page.kt
@@ -1,20 +1,31 @@
 package com.example.jis_lms
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.jis_lms.databinding.ActivityLoginPageBinding
 
 class login_page : AppCompatActivity() {
+    private val binding : ActivityLoginPageBinding by lazy{
+        ActivityLoginPageBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login_page)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.submitLoginPage.setOnClickListener{
+            startActivity(Intent(this , MainActivity::class.java))
+        }
+
     }
 }
